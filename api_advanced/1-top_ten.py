43bfe64ff72e84ff0062d3129dc0b666cc4016f2
Songa7273 <k.gaju@alustudent.com>
2025-02-25 06:38:05 -0800

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,17 +1,35 @@
 #!/usr/bin/python3
-"""Prints the title of the first 10 hot posts listed for a given subreddit"""
-
+"""
+Module to query the Reddit API and print the titles of the first 10 hot posts
+"""
 import requests
 
-
 def top_ten(subreddit):
-    """Main function"""
-    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts 
+    of a given subreddit.
 
-    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
-    try:
-        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
-        HOT_POSTS = RESPONSE.json().get("data").get("children")
-        [print(post.get('data').get('title')) for post in HOT_POSTS]
-    except Exception:
+    Args:
+        subreddit (str): The subreddit to search.
+    
+    Returns:
+        None
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "my_reddit_script/1.0"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    # Check if subreddit exists
+    if response.status_code != 200:
         print(None)
+        return
+
+    try:
+        data = response.json()
+        posts = data["data"]["children"]
+        for post in posts:
+            print(post["data"]["title"])
+    except KeyError:
+        print(None)
+
