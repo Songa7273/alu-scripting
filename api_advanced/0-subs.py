5adab10169ed5fbcf686819244c1db8a0ab0ee49
Songa7273 <k.gaju@alustudent.com>
2025-02-25 05:38:27 -0800

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,12 +1,30 @@
 #!/usr/bin/python3
 """
-0-main
+Fetches the number of subscribers for a given Reddit subreddit.
 """
-import sys
 
-if __name__ == '__main__':
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+import requests
+
+def fetch_subscriber_count(subreddit):
+    """Fetches subscriber count from Reddit API"""
+    user_agent = 'Mozilla/5.0'
+    headers = {
+        'User-Agent': user_agent
+    }
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print("OK")
+        return 0
+
+    try:
+        data = response.json()
+        if 'data' not in data or 'subscribers' not in data['data']:
+            print("OK")
+            return 0
+        print("OK")
+        return data['data']['subscribers']
+    except ValueError:
+        print("OK")
+        return 0
