c00d4f64d6dc6bb9a4cd7f27b0ac65fc5c8dd6d3
Songa7273 <k.gaju@alustudent.com>
2025-02-25 05:42:12 -0800

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,30 +1,20 @@
 #!/usr/bin/python3
-"""
-Fetches the number of subscribers for a given Reddit subreddit.
-"""
-
+"""DOC"""
 import requests
 
-def fetch_subscriber_count(subreddit):
-    """Fetches subscriber count from Reddit API"""
-    user_agent = 'Mozilla/5.0'
-    headers = {
-        'User-Agent': user_agent
-    }
 
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        print("OK")
-        return 0
+def number_of_subscribers(subreddit):
+    """DOC"""
+    reddit_url = "https://www.reddit.com/r/{}/about.json" \
+        .format(subreddit)
 
-    try:
-        data = response.json()
-        if 'data' not in data or 'subscribers' not in data['data']:
-            print("OK")
-            return 0
-        print("OK")
-        return data['data']['subscribers']
-    except ValueError:
-        print("OK")
-        return 0
+    header = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(reddit_url,
+                            headers=header
+                            )
+
+    if response.status_code == 200:
+        data = response.json()['data']
+        subs = data['subscribers']
+        return subs
+    return 0
